a5e346130f98ad633d503c9093d0b1eb23b40e27
yerim <sontt4266@naver.com>
2024-05-16 01:40:44 +0900

    [Test] BlogApiControllerTest - 글 조회 API 테스트

--- src/main/java/yerim/son/aimhightojava/controller/BlogApiController.java
+++ src/main/java/yerim/son/aimhightojava/controller/BlogApiController.java
@@ -3,10 +3,7 @@ package yerim.son.aimhightojava.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import yerim.son.aimhightojava.domain.Article;
 import yerim.son.aimhightojava.dto.AddArticleRequest;
 import yerim.son.aimhightojava.dto.ArticleResponse;
@@ -37,5 +34,13 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticles(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
     }
 }
