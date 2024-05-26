d8528dbc8aebd6449383b236cf9a16b068ee5f1a
yerim <sontt4266@naver.com>
2024-05-26 22:49:03 +0900

    [화면구성] 타임리프 글 목록 뷰 구현

---
+++ src/main/java/yerim/son/aimhightojava/controller/BlogViewController.java
@@ -0,0 +1,26 @@
+package yerim.son.aimhightojava.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import yerim.son.aimhightojava.dto.ArticleListViewResponse;
+import yerim.son.aimhightojava.service.BlogService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
---
+++ src/main/java/yerim/son/aimhightojava/dto/ArticleListViewResponse.java
@@ -0,0 +1,18 @@
+package yerim.son.aimhightojava.dto;
+
+import lombok.Getter;
+import yerim.son.aimhightojava.domain.Article;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
