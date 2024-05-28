89ef6a05e9d2c59e9ddfaf0eee249d6a62756a5a
yerim <sontt4266@naver.com>
2024-05-29 00:29:09 +0900

    [화면구성] 타임리프 글 뷰 구현

--- src/main/java/yerim/son/aimhightojava/controller/BlogViewController.java
+++ src/main/java/yerim/son/aimhightojava/controller/BlogViewController.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import yerim.son.aimhightojava.domain.Article;
 import yerim.son.aimhightojava.dto.ArticleListViewResponse;
+import yerim.son.aimhightojava.dto.ArticleViewResponse;
 import yerim.son.aimhightojava.service.BlogService;
 
 import java.util.List;
@@ -22,5 +25,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
